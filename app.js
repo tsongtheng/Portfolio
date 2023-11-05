ccd0317a65a6adae6d7978a543d008384d1b0aa2
Tipchan Songtheng <tipchans777@gmail.com>
2023-11-05 20:16:53 +0530

    add contact form functionality

--- app.js
+++ app.js
@@ -11,3 +11,28 @@
     document.body.classList.toggle("light-mode");
   });
 })();
+
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const subjectInput = document.getElementById("subject");
+const messageInput = document.getElementById("message");
+const sentMessage = document.getElementById("message-sent");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Clear input field
+  nameInput.value = "";
+  emailInput.value = "";
+  subjectInput.value = "";
+  messageInput.value = "";
+
+  // Display message sent
+  sentMessage.style.display = "block";
+
+  // Hide message after few seconds
+  setTimeout(() => {
+    sentMessage.style.display = "none";
+  }, 2500);
+});
