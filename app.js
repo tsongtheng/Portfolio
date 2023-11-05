e9bbf98d9eb5531e6233d01e49b44c8d531a492d
Tipchan Songtheng <tipchans777@gmail.com>
2023-11-06 02:55:31 +0530

    add form validation to form input

--- app.js
+++ app.js
@@ -16,17 +16,44 @@ const form = document.getElementById("form");
 const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const subjectInput = document.getElementById("subject");
-const messageInput = document.getElementById("message");
+const messageTextarea = document.getElementById("message");
 const sentMessage = document.getElementById("message-sent");
+const invalidInput = document.querySelectorAll(".invalid");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+});
 
-  // Clear input field
+const validateForm = () => {
+  // Check if the name input is empty
+  if (nameInput.value.trim() === "") {
+    nameInput.focus();
+    return false;
+  }
+
+  // Check if the email input is valid
+  if (!isEmailValid(emailInput.value)) {
+    emailInput.focus();
+    return false;
+  }
+
+  // Check if the subject input is empty
+  if (subjectInput.value.trim() === "") {
+    subjectInput.focus();
+    return false;
+  }
+
+  // Check if the message textarea is empty
+  if (messageTextarea.value.trim() === "") {
+    messageTextarea.focus();
+    return false;
+  }
+
+  // Clear input values
   nameInput.value = "";
   emailInput.value = "";
   subjectInput.value = "";
-  messageInput.value = "";
+  messageTextarea.value = "";
 
   // Display message sent
   sentMessage.style.display = "block";
@@ -34,5 +61,14 @@ form.addEventListener("submit", (event) => {
   // Hide message after few seconds
   setTimeout(() => {
     sentMessage.style.display = "none";
-  }, 2500);
-});
+  }, 3000);
+
+  // All fields are valid
+  return true;
+};
+
+const isEmailValid = (email) => {
+  const regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+};
